2d6db79686aee2709af1179dbf718d6a0558f3d1
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-03-22 17:23:59 -0400

    Time: 3 ms (69.07%), Space: 10.8 MB (28.15%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>>v;
+       void generate(vector<int>&subset,int i,vector<int>&nums)
+    {
+        if(i==nums.size())
+        {
+           v.push_back(subset);
+            return;
+        }
+           generate(subset,i+1,nums);
+           subset.push_back(nums[i]);
+           generate(subset,i+1,nums);
+           subset.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<int>subset;
+        generate(subset,0,nums);
+        return v;
+    }
+};
