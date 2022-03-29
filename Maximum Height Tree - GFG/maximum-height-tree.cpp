0fedb86f2cc8c7d7b86219e454bb8635406fa56c
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-03-29 11:32:35 -0400

    Added solution - LeetHub

---
+++ Maximum Height Tree - GFG/maximum-height-tree.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int n)
+    {
+      int c=1; 
+      int h=1;
+      while(h<=n)
+      {
+          h+=c+1;
+          c+=1;
+      }
+      return c-1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
