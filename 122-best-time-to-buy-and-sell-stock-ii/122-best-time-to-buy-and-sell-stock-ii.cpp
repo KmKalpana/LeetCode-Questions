1166106e99bfd72676421790756c2f8a85c5b192
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-06-02 04:17:28 -0400

    Time: 11 ms (45.95%), Space: 13.1 MB (53.79%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(int ind, int n, vector<int>&prices, int buy, vector<vector<int>>&dp)
+    /*int helper(int ind, int n, vector<int>&prices, int buy, vector<vector<int>>&dp)
     {
         if(ind==n)
             return 0;
@@ -16,11 +16,22 @@ public:
          profit=max(prices[ind]+helper(ind+1,n,prices,1,dp),0+helper(ind+1,n,prices,0,dp));
         }
         return dp[ind][buy]=profit;
-    }
-    int maxProfit(vector<int>& prices) 
+    }*/
+    int maxProfit(vector<int>& a) 
     {
-      int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-       return helper(0,n,prices,1,dp);
+      //int n=prices.size();
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+       //return helper(0,n,prices,1,dp);
+        if(a.size()==1)
+            return 0;
+        int profit=0;
+        for(int i=1; i<a.size(); i++)
+        {
+          if(a[i]>a[i-1])
+          {
+              profit+=(a[i]-a[i-1]);
+          }
+        }
+        return profit;
     }
 };
