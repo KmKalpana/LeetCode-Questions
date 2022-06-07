1139fef8bd02b35fc865de39814fcfe604e6a0ef
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-06-07 03:10:02 -0400

    Time: 3 ms (97.60%), Space: 10.4 MB (25.22%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -3,7 +3,20 @@ public:
     bool checkIfExist(vector<int>& arr) 
     {
       int n=arr.size();
-        for(int i=0; i<n; i++)
+        unordered_map<int,int>mp;
+      for(int i=0; i<n; i++)
+      {
+          if(mp.find(arr[i]*2)!=mp.end())
+          {
+              return true;
+          }
+          if(arr[i]%2==0 && mp.find(arr[i]/2)!=mp.end())
+          {
+              return true;
+          }
+          mp[arr[i]]++;
+      }
+      /*  for(int i=0; i<n; i++)
         {
             if(arr[i]%2==0)
             {
@@ -15,7 +28,7 @@ public:
                 }
             }
             }
-        }
+        }*/
         return false;
     }
 };
