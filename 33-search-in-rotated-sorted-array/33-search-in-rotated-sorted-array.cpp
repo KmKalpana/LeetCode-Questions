a32b856e7c0f5fb7cc419436d3c7473f4d6a8d51
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-07-16 22:13:35 +0530

    Time: 7 ms (52.53%), Space: 11.1 MB (29.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
-   
+   int helper(vector<int>&arr , int start, int last, int key)
+{
+    if(start>last)
+    {
+        return -1;
+    }
+    int mid=(start+last)/2;
+    if(arr[mid]==key)
+    {
+        return mid;
+    }
+     if(arr[start]<=arr[mid])
+     {
+         if(arr[start]<=key && arr[mid]>=key)
+         {
+             return helper(arr,start, mid-1, key);
+         }
+         else
+         {
+             return helper(arr, mid+1, last, key);
+         }
+     }
+    else
+    {
+        if(arr[last]>=key && arr[mid]<=key)
+        {
+            return helper(arr, mid+1, last, key);
+        }
+        else
+        {
+            return helper(arr, start, mid-1, key);
+        }
+    }
+}
     int search(vector<int>& nums, int target)
     {
         int start=0, end=nums.size()-1;
-        while(start<=end)
+        return helper(nums,start, end, target);
+      /*  while(start<=end)
         {
             int mid=(end+start)/2;
             if(nums[mid]==target)
             {
                 return mid;
             }
-           else if(nums[start]<=nums[mid])
+           else if(nums[start]<nums[mid])
            {
                if(nums[start]<=target && nums[mid]>target)
                { 
@@ -23,15 +57,15 @@ public:
             else 
             {
             if(nums[end]>=target && nums[mid]<target)
-            {
+             {
              start=mid+1;
-            }
+             }
             else 
             {
                 end=mid-1;
             }
         }
         }
-        return -1;
+        return -1;*/
     }
 };
