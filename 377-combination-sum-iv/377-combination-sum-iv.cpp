8c1fdc0f1c19a4581dc60924d167175bcfb78556
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-08-06 08:53:46 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-   int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int> result(target + 1);
-        result[0] = 1;
-        for (int i = 1; i <= target; ++i) {
-            for (int x : nums) {
-                if (i >= x) result[i] += result[i - x];
-            }
+    int helper(vector<int>&nums, int target, vector<int>&dp)
+    {
+       if(target<0)
+           return 0;
+        if(target==0)
+            return 1;
+        int ans=0;
+        if(dp[target]!=-1)
+            return dp[target];
+        for(int i=0; i<nums.size(); i++)
+        {
+            ans+=helper(nums,target-nums[i],dp);
         }
-        
-        return result[target];
+        return dp[target]=ans;
+    }
+    int combinationSum4(vector<int>& nums, int target) 
+    {
+        int n=nums.size();
+        vector<int>dp(target+1,-1);
+        int ans=helper(nums,target,dp);
+        return ans;
     }
 };
