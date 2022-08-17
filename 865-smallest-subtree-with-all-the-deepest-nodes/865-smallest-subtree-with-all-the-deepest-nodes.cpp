69ed717517f4c12d542a197f80023a57826b35c0
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-08-17 11:40:10 +0530

    Time: 15 ms (15.75%), Space: 14.4 MB (45.70%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,32 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    pair<TreeNode*,int> height(TreeNode* root,int h)
-    {
-      if(!root) return {root,0};
-      auto l=height(root->left,h+1);
-      auto r=height(root->right,h+1);
-       if(l.second==r.second)
-           return {root,l.second+1};
-        if(l.second>r.second)
-        {
-            cout<<"root->left "<<l.first->val<<"  Height is "<<l.second+1<<endl;
-            return {l.first,l.second+1};
-        }
-        return {r.first,r.second+1};
+    TreeNode* lca( TreeNode* root, TreeNode* p, TreeNode* q ) {
+        if ( !root || root == p || root == q ) return root;
+        TreeNode *left = lca( root->left, p, q );
+        TreeNode *right = lca (root->right, p, q );
+
+        return !left? right: !right? left: root;
     }
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) 
-    {
-         return height(root,0).first;
+    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        if ( !root || !root->left && !root->right ) return root;
+        TreeNode *leftMost = NULL;
+        TreeNode *rightMost = NULL;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        while( !q.empty() ) {
+            int levelSize = q.size();
+            for(int level = 0; level < levelSize; level++ ) {
+                TreeNode* node = q.front(); q.pop();
+                if ( level == 0 ) leftMost = node;
+                if ( level == levelSize - 1 ) rightMost = node;
+                
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+                
+            }
+        }
+        return lca( root, leftMost, rightMost );
     }
 };
