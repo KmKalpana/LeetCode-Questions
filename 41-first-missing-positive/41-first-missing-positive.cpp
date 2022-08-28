7049974b8dd9211a88084b0c0b5a40b014ef19cf
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-08-28 09:50:34 +0530

    Time: 89 ms (91.44%), Space: 31.4 MB (90.06%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) 
     {
-      unordered_set<int>st;
+     /* unordered_set<int>st;
         for(auto it: nums)
         {
             if(it>0)
@@ -13,6 +13,25 @@ public:
            if(st.find(i)==st.end())
                return i;
         }
-        return -1;
+        return -1;*/
+        int n=nums.size();
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]>=1 && nums[i]<=n)
+            {
+               int chair=nums[i]-1;
+                if(nums[chair]!=nums[i])
+                {
+                    swap(nums[chair],nums[i]);
+                    i--;
+                }
+            }
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(i+1!=nums[i])
+                return i+1;
+        }
+        return n+1;
     }
 };
