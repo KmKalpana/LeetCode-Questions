0b93cd81a18e1bae9487bc9afe9c22fbab83028a
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-08-29 20:32:03 +0530

    Time: 21 ms (39.17%), Space: 9.5 MB (27.79%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    typedef pair<int,int>pi;
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) 
+    {
+      vector<vector<int>>ans;
+       int n=mat.size();
+       int m=mat[0].size();
+       map<int,priority_queue<int,vector<int>,greater<int>>>pq;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+               pq[i-j].push(mat[i][j]);
+            }
+      }
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                mat[i][j]=pq[i-j].top();
+                pq[i-j].pop();
+            }
+        }
+        return mat; 
+    }
+};
