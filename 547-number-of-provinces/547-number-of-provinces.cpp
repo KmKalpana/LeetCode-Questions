52cc0f21beb65e83d72c6a70912a43ff3c2f93ae
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-09-21 23:40:55 +0530

    Time: 58 ms (19.53%), Space: 13.8 MB (67.93%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -15,7 +15,7 @@ public:
         int count=n;
         for(int i=0; i<n; i++)
         {
-            for(int j=i+1; j<n; j++)
+            for(int j=0; j<i; j++)
             {
                 if(isConnected[i][j])
                 {
