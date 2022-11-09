4d46a6936493ab9e8fc7d1f3f4c134f8b6a839e5
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-11-09 14:22:06 +0530

    Time: 24 ms (33.05%), Space: 10.5 MB (6.40%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -24,24 +24,37 @@ public:
       int n=grid.size();
       int m=grid[0].size();
       vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0);
     for(int i=0; i<n; i++)
     {
+        vector<int>cur(m,0);
         for(int j=0; j<m; j++)
         {
             if(i==0 && j==0)
             {
-               dp[i][j]=grid[i][j];
+               //dp[i][j]=grid[i][j];
+                cur[j]=grid[i][j];
             }
             else 
             {
                 int up=INT_MAX, left=INT_MAX;
-              if(i>0)  up=grid[i][j]+dp[i-1][j];
-                if(j>0) left=grid[i][j]+dp[i][j-1];
-                dp[i][j]=min(up,left);
+                if(i>0) 
+                {
+                    // up=grid[i][j]+dp[i-1][j];
+                    up=prev[j]+grid[i][j];
+                }
+                if(j>0) 
+                {
+                    //left=grid[i][j]+dp[i][j-1];
+                    left=cur[j-1]+grid[i][j];
+                }
+               // dp[i][j]=min(up,left);
+                cur[j]=min(up,left);
             }
         }
+        prev=cur;
     }
-       return dp[n-1][m-1];
-       
+      // return dp[n-1][m-1];
+      return prev[m-1]; 
     }
 };
