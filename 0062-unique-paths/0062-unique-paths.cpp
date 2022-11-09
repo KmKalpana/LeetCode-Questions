8fe2652bc14f939a7b246117f4142b63dbdd7a35
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-11-09 12:37:57 +0530

    Time: 2 ms (52.58%), Space: 6.1 MB (64.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     //Recursion TC=2^M*N  sc=0((m-1)+(n-1))
     //Memoization TC=0(M*N)  sc=((m-1)+(n-1))+dp
+    //Tabular TC=0(M*N)  sc=dp
     int solve(int m , int n, vector<vector<int>>&dp)
     {
         if(m==0 && n==0)
@@ -23,25 +24,30 @@ public:
         }
     int uniquePaths(int m, int n) 
     {
-        vector<vector<int>>dp(m,vector<int>(n,0));
+       // vector<vector<int>>dp(m,vector<int>(n,0));
        // return solve(m-1,n-1,dp);
+        vector<int>prev(n,0);
         for(int i=0; i<m; i++)
         {
+            vector<int>cur(n,0);
             for(int j=0; j<n; j++)
             {
-                if(i==0 and j==0)
-                    dp[i][j]=1;
+                if(i==0 and j==0) cur[j]=1;
+                   // dp[i][j]=1; 
                 else
                 {
-                int right=0,down=0;
-                if(i>0) 
-                 right=dp[i-1][j];
-                if(j>0) 
-                  down=dp[i][j-1];
-               dp[i][j]=right+down; 
+                int left=0,up=0;
+                if(i>0)  up=prev[j];
+                 //up=dp[i-1][j];
+                if(j>0)  left=cur[j-1];
+                //  left=dp[i][j-1];
+                //  dp[i][j]=up+left; 
+               cur[j]=up+left;
                 }
             }
+            prev=cur;
         }
-        return dp[m-1][n-1];
+      //  return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
