d504bac360f09dec03f8bbcdd66d350305d35db2
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-11-09 15:34:46 +0530

    Time: 16 ms (33.90%), Space: 8.6 MB (78.77%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -15,21 +15,28 @@ public:
         int n=triangle.size();
     //   vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
       //  return helper(triangle,0,0,n,dp);
-        vector<vector<int>> dp(n,vector<int>(n,0)) ;
+        vector<int>front(n,0),cur(n,0);
+        //vector<vector<int>> dp(n,vector<int>(n,0)) ;
        for(int i=0; i<n; i++)
        {
-           dp[n-1][i]=triangle[n-1][i];
+           //dp[n-1][i]=triangle[n-1][i];
+           front[i]=triangle[n-1][i];
        }
         for(int i=n-2; i>=0; i--)
         {
             for(int j=i; j>=0; j--)
             {
-                  int left=1e5,up=1e5;
-                  left=dp[i+1][j+1]+triangle[i][j];
-                  up= dp[i+1][j]+triangle[i][j]; 
-                dp[i][j]=min(left,up);
+                  
+                //int left=dp[i+1][j+1]+triangle[i][j];
+                int down=front[j]+triangle[i][j];
+                // int up= dp[i+1][j]+triangle[i][j]; 
+                int right=front[j+1]+triangle[i][j];
+                //dp[i][j]=min(left,up);
+                cur[j]=min(down,right);
             }
+            front=cur;
         }
-        return dp[0][0];
+      //  return dp[0][0];
+        return front[0];
     }
 };
