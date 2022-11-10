2d145899424dacca0213b24dcb8211adc3084600
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-11-11 00:45:26 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -10,10 +10,11 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     int helper(int ind, int MW, int wt[], int val[], vector<vector<int> >&dp)
     {
-       // if(ind==0)
-        ///{
-         //if(wt[0]<=MW) return val[0];
-    //    }
+        //SC=O(N*W) + O(N)  //0(N)=Stack size
+        if(ind==0)
+        {
+         if(wt[0]<=MW) return val[0];
+        }
         if(ind<0) return 0;
         if(dp[ind][MW]!=-1) return dp[ind][MW];
         int not_Take=0+helper(ind-1,MW,wt,val,dp);
@@ -24,15 +25,33 @@ class Solution
     }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       vector<vector<int> >dp(n,vector<int>(W+1,-1));
-      int ans= helper(n-1,W,wt,val,dp);
-    //   for(int i=0; i<n; i++)
-    //   {
-    //       for(int j=0; j<W; j++)
-    //       cout<<dp[i][j]<<" ";
-    //       cout<<endl;
-    //   }
-       return ans;
+       vector<vector<int> >dp(n,vector<int>(W+1,0));
+     // int ans= helper(n-1,W,wt,val,dp);
+   vector<int>prev(W+1,0),cur(W+1,0);
+    for(int w=wt[0]; w<=W; w++)
+    {
+       // dp[0][w]=val[0];
+        prev[w]=val[0];
+    }
+    // Space optimization
+  
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0; j<=W; j++)
+        {
+            //int not_take=dp[i-1][j];
+            int not_take=prev[j];
+            int take=INT_MIN;
+            if(wt[i]<=j)
+              //take=dp[i-1][j-wt[i]]+val[i];
+              take=prev[j-wt[i]]+val[i];
+            //dp[i][j]=max(not_take,take);
+            cur[j]=max(not_take,take);
+        }
+        prev=cur;
+    }
+     //  return dp[n-1][W];
+     return prev[W];
     }
 };
 
