4002af2ae83b25e4471ac22fd88d83b86a9e9ded
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-11-12 15:21:01 +0530

    Time: 6 ms (74.36%), Space: 12 MB (35.51%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int helper(int ind, int buy, vector<int>&prices,vector<vector<int>>&dp)
-    {
-        if(ind>=prices.size())
-            return 0;
-        if(dp[ind][buy]!=-1)
-            return dp[ind][buy];
-        int profit=0;
-        if(buy==1){
-          profit=max(helper(ind+1,1,prices,dp), -prices[ind]+helper(ind+1,0,prices,dp));
-        }
-        if(buy==0)
-        {
-        profit=max(helper(ind+1,0,prices,dp),prices[ind]+helper(ind+2,1,prices,dp));  
-        }
-        return dp[ind][buy]=profit;
-    }
+    // int helper(int ind, int buy, vector<int>&prices,vector<vector<int>>&dp)
+    // {
+    //     if(ind>=prices.size())
+    //         return 0;
+    //     if(dp[ind][buy]!=-1)
+    //         return dp[ind][buy];
+    //     int profit=0;
+    //     if(buy==1){
+    //       profit=max(helper(ind+1,1,prices,dp), -prices[ind]+helper(ind+1,0,prices,dp));
+    //     }
+    //     if(buy==0)
+    //     {
+    //     profit=max(helper(ind+1,0,prices,dp),prices[ind]+helper(ind+2,1,prices,dp));  
+    //     }
+    //     return dp[ind][buy]=profit;
+    // }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
-       return helper(0,1,prices,dp); 
+       //  vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
+       // return helper(0,1,prices,dp); 
+         int n=prices.size();
+         vector<vector<int>> dp(n+2,vector<int>(2,0));
+           for(int ind=n-1; ind>=0; ind--)
+        {
+            for(int buy=0; buy<=1; buy++)
+            {
+                    if(buy==0)
+                    {
+                        dp[ind][buy]=max(dp[ind+1][0],(-prices[ind]+dp[ind+1][1]));
+                    }
+                    if(buy==1)
+                    {
+                      dp[ind][buy]=max(dp[ind+1][1],prices[ind]+dp[ind+2][0]);
+                  
+                    }
+            }
+        }
+        return dp[0][0];
     }
 };
