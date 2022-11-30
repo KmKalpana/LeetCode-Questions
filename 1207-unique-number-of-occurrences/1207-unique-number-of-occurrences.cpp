3e1bb115a613c48c7fa6f4d6da7da4d212d84b39
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-11-30 08:14:57 +0530

    Time: 4 ms (69.41%), Space: 8.2 MB (20.75%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) 
+    {
+         unordered_map<int,int>mp;
+        for(auto it: arr)
+        {
+            mp[it]++;
+        }
+        set<int>st;
+        for(auto it: mp)
+        {
+            st.insert(it.second);
+        }
+        return mp.size()==st.size();
+    }
+};
