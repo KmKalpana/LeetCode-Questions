21cbd1b65a91cc74edef99eee6785161718f3c67
Kalpana Kathait <85006350+KmKalpana@users.noreply.github.com>
2022-12-05 13:52:57 +0530

    Time: 189 ms (88.04%), Space: 42.4 MB (72.99%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    //Time Complexit= (n^2 * logn)
     int minCostConnectPoints(vector<vector<int>>& points)
     {
        int res=0, i=0, loop=1, n=points.size();
-        priority_queue<pair<int,int>>pq;  
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;  //mean heap
+       // priority_queue<pair<int,int>>pq;  //max heap
+        
         vector<int>vis(n,0);
         while(loop++<n)
         {
@@ -12,12 +15,13 @@ public:
             {
                 if(!vis[j])
                 {
-                    pq.push({-(abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])),j});
+                    pq.push({(abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1])),j});
                 }
             }
             while(vis[pq.top().second])
               pq.pop();
-           res-=pq.top().first;
+             res+=pq.top().first;    //min heap
+           //  res-=pq.top().first;    //max heap
             i=pq.top().second;
            pq.pop();
         }
